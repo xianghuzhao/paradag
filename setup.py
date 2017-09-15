935c9952755204218077be645f3c26f5a04ebf04
Xianghu Zhao <xianghuzhao@gmail.com>
2017-09-15 12:39:08 +0800

    Stable status in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license = 'MIT',
 
     classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
 
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
